42225891c8497797a7ac16813f950e775caee7aa
Zane <i.zane.am.robot@gmail.com>
2017-06-22 20:53:46 -0400

    Started building the nested form

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -13,6 +13,7 @@ class QuizzesController < ApplicationController
   # GET /quizzes/new
   def new
     @quiz = Quiz.new
+    @quiz.questions.build
   end
 
   # GET /quizzes/1/edit
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,6 @@ class Question < ApplicationRecord
   belongs_to :quiz
   has_many :answers
   has_one :correct_answer
+  accepts_nested_attributes_for :answers
+  accepts_nested_attributes_for :correct_answer
 end
